056053430637e36256a5c15b595c14bd7c2f5123
mcreseau <viannijeu@gmail.com>
2023-04-03 12:34:41 +0100

    Adding insert.sql file

---
+++ insert.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS personne(
+  names varchar(255) NOT NULL
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  INSERT INTO personne(names) VALUES('TENNE');
+  INSERT INTO personne(names) VALUES('NOUTENIJEU');
+  INSERT INTI personne(names) VALUES('Vianney');
