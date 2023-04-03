9ba0b9251e225fd645774dd3d51298faf86e9fc9
mcreseau <viannijeu@gmail.com>
2023-04-03 12:49:32 +0100

    Fixing some issues in insert.sql file

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS personne(
+CREATE TABLE IF NOT EXISTS personne (
   names varchar(255) NOT NULL
   ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
   INSERT INTO personne(names) VALUES('TENNE');
   INSERT INTO personne(names) VALUES('NOUTENIJEU');
-  INSERT INTI personne(names) VALUES('Vianney');
+  INSERT INTO personne(names) VALUES('Vianney');
